8eeb1a2812607abc8cc6c113676ba14db6827091
Dane Murphy <danem7@gmail.com>
2021-02-25 00:30:49 -0800

    using domain address for email verification

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -25,13 +25,13 @@ export class AuthService {
         );
     }
 
-    public sendVerificationEmail(activationPassword: string) {
-        const verificationLink = `http://localhost:3000/verify/${activationPassword}`;
+    public sendVerificationEmail(activationPassword: string, email: string) {
+        const verificationLink = `${process.env.DOMAIN}/verify/${activationPassword}`;
 
         sgMail.setApiKey(process.env.SENDGRID_API as string);
         const msg = {
-            to: process.env.TEST_EMAIL,
-            from: process.env.ETHEREAL_EMAIL as string,
+            to: process.env.TEST_EMAIL || email,
+            from: process.env.LOGGER_EMAIL as string,
             subject: 'Verify your account',
             html: `<p>Click the link to verify your account: ${verificationLink}</p>`,
         };
